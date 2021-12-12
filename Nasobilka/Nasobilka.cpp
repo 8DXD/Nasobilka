fd6f8fcf17357c5166a738b577586f64cbfecd56
Kateřina Dvořáková <kacule05@gmail.com>
2021-12-12 20:36:56 +0100

    Přidejte soubory projektu.

---
+++ Nasobilka/Nasobilka.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+int main()
+{
+    int a = 1; 
+    int b = 1;
+    printf("Mala nasobika:\n");
+    while (a <= 10) {
+        for (b = 1; b <= 10; b++) {
+            printf("%d ", b * a);
+        }
+        printf("\n");
+        int b = 1;
+        a++;
+    }
+}
